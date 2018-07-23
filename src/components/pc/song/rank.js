a82488aa7e25b1054f3e8e9f5a1759b1e29d3e6a
wang-qingqing <sa514104@mail.ustc.edu.cn>
2018-07-23 20:38:46 +0800

    update

--- src/components/pc/song/rank.js
+++ src/components/pc/song/rank.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {
+    Collapse,
+    Icon
+  } from 'antd';
+const {Panel} = Collapse;
 
 export default class SongRank extends React.Component{
     render(){
         return (
-            <div>      
-                SongRank 
-            </div>
+            <Collapse>
+                <Panel style={{position: 'relative'}} header="听歌排行">
+                    <a className="showMoreList a-style">
+                        最近一周
+                    </a>
+                                                    
+                    
+                </Panel>
+            </Collapse>  
         )
     }
 }
